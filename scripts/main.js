255256c57e9b74afbf1a9a7c673037ffd21087bf
bandura <banduraweb@ukr.net>
2019-10-26 13:29:35 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+(() => {
+  const today = new Date();
+  const innersForDays = document.body.getElementsByTagName('div');
+  function getDInW() {
+    return {
+      1: 'mon',
+      2: 'tue',
+      3: 'wed',
+      4: 'thu',
+      5: 'fri',
+      6: 'sat',
+      7: 'sun',
+    };
+  }
+  for (let i = 0; i < innersForDays.length; i++) {
+    if (i === today.getDate()) {
+      innersForDays[i].style.cssText = 'background-color: #4bcaff;';
+      innersForDays[i].innerHTML = `<span>${getDInW()[today.getDay()]}</span>`;
+      document.body.getElementsByTagName('span')[0]
+        .style.cssText = 'position: relative; left:-5px';
+    }
+  }
+})();
