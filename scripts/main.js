9234aa1386993d7277245cce694651a01b2f72f6
bandura <banduraweb@ukr.net>
2019-11-14 01:16:18 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello world');
+
+
+  const btnGetStarted = document.getElementById('button');
+  const bg = document.getElementById('gray');
+  const windowPlay = document.getElementById('modal-form');
+  btnGetStarted.addEventListener('click', modalWindow);
+   function modalWindow() {
+     bg.style.display = 'block';
+     setTimeout(function () {
+       windowPlay.style.display = 'block';
+     },3000)
+   }
+
+
